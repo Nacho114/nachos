48a2be379e7a0463682bbe55d3c9aa7be2994fc1
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-08-04 10:12:35 +0200

    latest

--- configuration.nix
+++ configuration.nix
@@ -189,6 +189,21 @@
     enableSSHSupport = true;
   };
 
+  programs.nix-ld.enable = true;
+
+  # Sets up all the libraries to load
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    fuse3
+    icu
+    nss
+    openssl
+    curl
+    expat
+    # ...
+  ];
+
   # ---------------------------------------------------------------------------
   # ---------------------------------------------------------------------------
   # ---------------------------------------------------------------------------
